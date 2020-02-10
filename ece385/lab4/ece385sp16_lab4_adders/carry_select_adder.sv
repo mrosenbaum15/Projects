a92cf31756883a6442e8771e6e6a24719e2b97f8
mrosenbaum15 <44303950+mrosenbaum15@users.noreply.github.com>
2020-02-10 16:48:50 -0600

    Update carry_select_adder.sv
    added comments

--- ece385/lab4/ece385sp16_lab4_adders/carry_select_adder.sv
+++ ece385/lab4/ece385sp16_lab4_adders/carry_select_adder.sv
@@ -12,15 +12,22 @@ module carry_select_adder
      * Your code should be completly combinational (don't use always_ff or always_latch).
      * Feel free to create sub-modules or other files. */
 	  
+	  // carry bits after each select adder (incremented by 4)
 	  logic C4, C8, C12;
 	  
+	  // first adder is just a 4-bit ripple adder...based off diagram on page 6 of lab4 manual
 	  four_bit_ripple final_select_0(.x(A[3:0]), .y(B[3:0]), .cin(1'b0), .sum(Sum[3:0]), .cout(C4));
+	
+	  // need three 4-bit select adders...based off diagram on page 6 on lab4 manual
 	  select_adder final_select_1(.x(A[7:4]), .y(B[7:4]), .cin(C4), .sum(Sum[7:4]), .cout(C8));
 	  select_adder final_select_2(.x(A[11:8]), .y(B[11:8]), .cin(C8), .sum(Sum[11:8]), .cout(C12));
 	  select_adder final_select_3(.x(A[15:12]), .y(B[15:12]), .cin(C12), .sum(Sum[15:12]), .cout(CO));
 		     
 endmodule
 
+// creating select adder
+// inputs: x, y (4 bits), cin (carry-in)
+// outputs: sum (4 bits), cout (carry-out)
 module select_adder
 (
 		input[3:0] x,
@@ -30,21 +37,28 @@ module select_adder
 		output logic cout
 );
 
-logic c0, c1;
-logic [3:0] sum_first;
-logic [3:0] sum_second;
+	logic [1:0] C; // two carry bits... one between each adder and one after second adder
+	logic [3:0] sum_first; // sum of first addder
+	logic [3:0] sum_second; // sum off adder
+	
+	// need two adders, seen within diagram on page 6 of lab4 manual
+	four_bit_ripple first_select_0(.x(x), .y(y), .cin(0), .sum(sum_first), .cout(C[0]));
+	four_bit_ripple first_select_1(.x(x), .y(y), .cin(1), .sum(sum_second), .cout(C[1]));
 
-four_bit_ripple first_select_0(.x(x), .y(y), .cin(0), .sum(sum_first), .cout(c0));
-four_bit_ripple first_select_1(.x(x), .y(y), .cin(1), .sum(sum_second), .cout(c1));
-
+	// need four MUXs as seen in diagram on page 6 of lab4 manual... one for each of the 4 sum bits
 two_one_MUX mux_0(.in0(sum_first[0]), .in1(sum_second[0]), .select(cin), .mux_output(sum[0]));
 two_one_MUX mux_1(.in0(sum_first[1]), .in1(sum_second[1]), .select(cin), .mux_output(sum[1]));
 two_one_MUX mux_2(.in0(sum_first[2]), .in1(sum_second[2]), .select(cin), .mux_output(sum[2]));
 two_one_MUX mux_3(.in0(sum_first[3]), .in1(sum_second[3]), .select(cin), .mux_output(sum[3]));
-two_one_MUX mux_4(.in0(c0), .in1(c1), .select(cin), .mux_output(cout));
+	
+	// last MUX replaces AND/OR gates after each select adder
+	two_one_MUX mux_4(.in0(C[0]), .in1(C[1]), .select(cin), .mux_output(cout));
 
 endmodule
 
+// creating simple 2:1 MUX
+// inputs: in0, in1 (data-in, 2 bits), select (1 bit)
+// output: mux_output (output result)
 module two_one_MUX
 (
 			input logic in0,
@@ -57,7 +71,8 @@ module two_one_MUX
 always_comb
 
 	begin
-	
+		
+		// determing which data bit should be selected
 		if(select == 1'b0)
 			mux_output = in0;
 		else
