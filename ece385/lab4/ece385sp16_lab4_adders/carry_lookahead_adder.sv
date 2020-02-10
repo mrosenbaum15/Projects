46b352f677364148e73ea9348765aebd07a2bd1d
mrosenbaum15 <44303950+mrosenbaum15@users.noreply.github.com>
2020-02-10 16:33:25 -0600

    Update carry_lookahead_adder.sv
    added comments

--- ece385/lab4/ece385sp16_lab4_adders/carry_lookahead_adder.sv
+++ ece385/lab4/ece385sp16_lab4_adders/carry_lookahead_adder.sv
@@ -12,19 +12,20 @@ module carry_lookahead_adder
      * Your code should be completly combinational (don't use always_ff or always_latch).
      * Feel free to create sub-modules or other files. */
 	  
-	  logic [4:0] C;
-	  logic [3:0] G;
-	  logic [3:0] P;
+	logic [3:0] C; // array of carry-out bits
+	logic [3:0] G; // generating logic
+	logic [3:0] P; // propagating logic
 	  
 	  always_comb begin
-	  
-			for(int i = 1; i < 5; i++) begin
+	  	  // setting carry out, based off given equation given on page 4 of lab4 manual
+		  for(int i = 1; i <= 5; i++) begin
 				C[i] = G[i-1] | (P[i-1] & C[i-1]);
 			end
-			C[0] = 0;
-			CO = C[4];
+		  C[0] = 0; // first carry-in is 0
+		  CO = C[4]; // final carry out is 
 	  end
 	  
+	  // carry lookahead adder consists of four 4-bit ripple adders
 	  four_bit_rip FBR0(.x(A[3:0]), .y(B[3:0]), .cin(0), .sum(Sum[3:0]), .g_last(G[0]), .p_last(P[0]));
 	  four_bit_rip FBR1(.x(A[7:4]), .y(B[7:4]), .cin(C[1]), .sum(Sum[7:4]), .g_last(G[1]), .p_last(P[1]));
 	  four_bit_rip FBR2(.x(A[11:8]), .y(B[11:8]), .cin(C[2]), .sum(Sum[11:8]), .g_last(G[2]), .p_last(P[2]));
@@ -32,6 +33,9 @@ module carry_lookahead_adder
 	  
 endmodule
 
+//creating four bit ripple adder
+// inputs: x, y (4 bits), cin (carry-in)
+// outputs: sum (4 bits), g_last, p_last (output signals)
 module four_bit_rip
 (
 			input[3:0] x,
@@ -42,26 +46,31 @@ module four_bit_rip
 			output logic p_last
 );
 
-logic [3:0] C;
-logic [3:0] G;
-logic [3:0] P;
+	logic [3:0] C; // carry-in bits between adders
+	logic [3:0] G; // generation logic
+	logic [3:0] P; // propagation logic
 
 always_comb begin
-
+	
+	// setting initial carry-in from input
 	C[0] = cin;
-
+	
+	// given on page 4 of lab4 manual
 	P = x ^ y;
 	G = x & y;
 	
+	// given on page 4 of lab4 manual
 	for(int i = 1; i < 4; i++) begin
 		C[i] = G[i-1] | (P[i-1] & C[i-1]);
 	end
 	
 end
-
+	
+// output signals given on page 4 of lab4 manual
 assign g_last = (G[3]) | (G[2] & P[3]) | (G[1] & P[3] & P[2]) | (G[0] & P[3] & P[2] & P[1]);
 assign p_last = P[3] & P[2] & P[1] & P[0];
 
+// four full_adders within a 4-bit ripple adder
 full_adder f0(.x(x[0]), .y(y[0]), .cin(C[0]), .sum(sum[0]));
 full_adder f1(.x(x[1]), .y(y[1]), .cin(C[1]), .sum(sum[1]));
 full_adder f2(.x(x[2]), .y(y[2]), .cin(C[2]), .sum(sum[2]));
