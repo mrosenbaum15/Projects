1f4de0fc0a155319a1af3ab631e75bc4ae7e5c65
mrosenbaum15 <44303950+mrosenbaum15@users.noreply.github.com>
2020-02-10 16:22:36 -0600

    Update ripple_adder.sv
    added comments

--- ece385/lab4/ece385sp16_lab4_adders/ripple_adder.sv
+++ ece385/lab4/ece385sp16_lab4_adders/ripple_adder.sv
@@ -11,9 +11,11 @@ module ripple_adder
      * Insert code here to implement a ripple adder.
      * Your code should be completly combinational (don't use always_ff or always_latch).
      * Feel free to create sub-modules or other files. */
-
+	  
+	  // carry bits between the adders
 	  logic C0, C1, C2;
 	  
+	  // creating four individual adders together to make it a 16-bit ripple adder
 	  four_bit_ripple FBR_0(.x(A[3:0]), .y(B[3:0]), .cin(0), .sum(Sum[3:0]), .cout(C0));
 	  four_bit_ripple FBR_1(.x(A[7:4]), .y(B[7:4]), .cin(C0), .sum(Sum[7:4]), .cout(C1));
 	  four_bit_ripple FBR_2(.x(A[11:8]), .y(B[11:8]), .cin(C1), .sum(Sum[11:8]), .cout(C2));
@@ -21,7 +23,9 @@ module ripple_adder
      
 endmodule
 
-
+// creating 4-bit ripple adder
+// inputs: x, y (4 bits each), cin (carry-in)
+// outputs: sum (4 bits), cout (carry-out)
 module four_bit_ripple
 (
 			input [3:0] x,
@@ -31,8 +35,10 @@ module four_bit_ripple
 			output logic cout
 );	
 
+// carry bits between adders
 logic c0, c1, c2;
 
+// each ripple adder has four 1-bit full adders
 full_adder full_one(.x(x[0]), .y(y[0]), .cin(cin), .sum(sum[0]), .cout(c0));
 full_adder full_two(.x(x[1]), .y(y[1]), .cin(c0), .sum(sum[1]), .cout(c1));
 full_adder full_three(.x(x[2]), .y(y[2]), .cin(c1), .sum(sum[2]), .cout(c2));
@@ -40,6 +46,7 @@ full_adder full_four(.x(x[3]), .y(y[3]), .cin(c2), .sum(sum[3]), .cout(cout));
 
 endmodule
 
+// based off given code in lab 4 manual
 module full_adder
 (
 			input x,
