0248046cda2520ac17accf20021a7e6b77d0e7cc
unknown <mer5@illinois.edu>
2020-02-24 15:19:16 -0600

    debugging now

--- ece385/lab6/mux.sv
+++ ece385/lab6/mux.sv
@@ -56,15 +56,13 @@ always_comb
 
 endmodule
 
-// creating simple 4:1 MUX with three bit input
-// inputs: in0, in1, in2, in3 (data-in, 3 bits each), select (2 bit)
+// creating simple 2:1 MUX with three bit input
+// inputs: in0, in1 (data-in, 3 bits each), select (2 bit)
 // output: mux_output (3-bit output result)
-module four_one_MUX_3bit
+module two_one_MUX_3bit
 (
 			input logic [2:0] in0,
 			input logic [2:0] in1,
-            input logic [2:0] in2,
-            input logic [2:0] in3,
 			input logic [1:0] select,
 			output logic [2:0] mux_output
 );
@@ -77,12 +75,8 @@ always_comb
 		// determing which data bit should be selected
 		if(select == 2'b00)
 			mux_output = in0;
-		else if(select == 2'b01)
+		else 
 			mux_output = in1;
-        else if(select == 2'b10)
-    		mux_output = in2;
-        else
-        	mux_output = in3;
 
 	end
 
